ee657d9132396a215efc359ca134e5c19e9a009d
LeoGale <leoogale@gmail.com>
2019-11-19 10:32:31 +0800

    Create check_has_member_func.h
    check if has member function at compile time 
    std::enable_if, veriatic template
    #C++ #template #SFINAE

---
+++ check_member/check_has_member_func.h
@@ -0,0 +1,14 @@
+#define DECLARE_HAS_MEMBER(NAME) \
+template<class T, typename... Args> \
+struct has_member_##NAME \
+{                           \
+  template<class U> \
+  constexpr static auto check(void*)->decltype(std::decval<T>().NAME(std::decval<Args>()...), std::true_type()); \
+  
+  template<class U> \
+  constexpr static std::false_type() check(...); \
+  static constexpr bool value = decltype(check<T>(nullptr))::value; \
+};
+
+#define HAS_CLASS_MEMBER(CLASS, MEMBER, ...) \
+has_member_##MEMBER(CLASS, __VA_ARGS__)::value;
